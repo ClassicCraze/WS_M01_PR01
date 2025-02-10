b80078bdbf7351e5594d2e65f6f7fae0e50ea15e
Wade Schrotenboer <schrotew@my.erau.edu>
2025-02-10 13:12:29 -0700

    Changed Invisible Pickup

--- My Hero Born/Assets/Scripts/UndercoverItemBehavior.cs
+++ My Hero Born/Assets/Scripts/UndercoverItemBehavior.cs
@@ -1,20 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.Collections; 
+using System.Collections.Generic; 
 using UnityEngine;
 
- public class UndercoverItemBehavior : MonoBehaviour 
- {
-       // 1
-     void OnCollisionEnter(Collision collision)
-     {
-         // 2
-         if(collision.gameObject.name == "Player")
-         {
-             // 3
-             Destroy(this.transform.parent.gameObject);
+public class UndercoverItem : MonoBehaviour
+{
+    public float invisibilityDuration = 10f;
 
-             // 4
-             Debug.Log("Undercover pickup collected!");
-         }
-     }
- }
+    public GameObject player;
+
+    private Renderer playerRenderer;
+
+    private Collider playerCollider;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerRenderer = player.GetComponent<Renderer>();
+        playerCollider = player.GetComponent<Collider>();
+    }
+
+    void OnTriggerEnter(Collider playerCollider)
+    {
+        if (playerCollider.CompareTag("Player"))
+        {
+            StartCoroutine(ActivateInvisibility());
+            
+            Destroy(gameObject);
+        }
+    }
+
+    private IEnumerator ActivateInvisibility()
+    {
+        playerRenderer.enabled = false;
+
+        yield return new WaitForSeconds(invisibilityDuration);
+
+        playerRenderer.enabled = true;
+    }
+}
